5758c9a518fb9b35f147e292ac29f2a121542009
Lawyne <tithnara.sun@ensta-bretagne.org>
2017-01-10 15:22:42 +0100

    Create handleLED.h

---
+++ handleLED.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <unistd.h>
+#include <time.h>
+#define TIME_DOT 100
+#define TIME_DASH 1000
+#define TIME_SPACE 500
+
+
+
+void readInt(int e);
+void readMorse(int* morse, int size, int led);
+
+write(led, 1, sizeof(int));
