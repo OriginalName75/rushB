554cf059a412de69954751fa599f7e994bb12965
Lawyne <tithnara.sun@ensta-bretagne.org>
2017-01-16 16:20:17 +0100

    Update cryptage.h

--- cryptage.h
+++ cryptage.h
@@ -1,4 +1,12 @@
-int find(char r, char* table); //Cryptage d'un caractère
-char* cryptage(char* r);  //Cryptage du message initial
-int defind(char r, char* table); //Décryptage d'un caractère
-char* decryptage(char* r); //Décryptage du message codé
+/*Use the Cesar cipher on the char c, 
+return c+3
+*/
+int find(char r, char* table);
+
+char* cryptage(char* r);  //Cypher the string r
+
+/*Use the Cesar cipher on the char c, 
+return c-3
+*/
+int defind(char r, char* table);
+char* decryptage(char* r); //Decypher the string r
