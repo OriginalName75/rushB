ecd11d6d4697ac31e2bcfd50ee2b4e56ed3239b2
Lawyne <tithnara.sun@ensta-bretagne.org>
2017-01-16 15:25:55 +0100

    Update handleLED.h

--- handleLED.h
+++ handleLED.h
@@ -6,7 +6,12 @@
 #define TIME_SPACE 500
 
 
-
+/*Gere le clignotement de la led en morse selon l'entier en input
+0 est un espace en morse
+2 est un point en morse
+3 est un trait en morse*/
 void readInt(int e, int led);
+
+/*Gere le clignotement de la led en morse pour un tableau d'entier morse en input*/
 void readMorse(int* morse, int size, int led);
 
