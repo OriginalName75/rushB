811f2de3630830f6c29a0342c88f740d12030924
Aubertot <kentin8112@gmail.com>
2017-01-16 16:03:42 +0100

    Update cryptage.h

--- cryptage.h
+++ cryptage.h
@@ -1,4 +1,4 @@
-int find(char r, char* table);
+int find(char r, char* table); //Cryptage d'un caractère
 char* cryptage(char* r);  //Cryptage du message initial
-int defind(char r, char* table);
+int defind(char r, char* table); //Décryptage d'un caractère
 char* decryptage(char* r); //Décryptage du message codé
